01e9cfe465c400aca75714e4b020df5ae01bda85
piercestar <kong_terence@hotmail.com>
2020-05-27 12:42:25 +0800

    update deployables

--- src/app/mock-server/mock-data/mock-reports.ts
+++ src/app/mock-server/mock-data/mock-reports.ts
@@ -1,65 +1,70 @@
 export let mockreports = [
   {
     report_id: 1,
-    name: 'Hydrogen',
-    created_dt: new Date(),
+    name: 'Henry',
+    created_dt: new Date(2020, 1, 4),
     status: 'approved',
-    modified_dt: new Date(2018, 11, 24, 10),
+    modified_dt: new Date(2020, 1, 17 ),
     content: `Hydrogen is a chemical element with status H and atomic number 1. With a standard \n atomic created_dt of 1.008, hydrogen is the lightest element on the periodic table.`
   }, {
     report_id: 2,
-    name: 'Helium',
-    created_dt: new Date(),
+    name: 'Henry',
+    created_dt: new Date(2020, 1, 17 ),
     status: 'rejected',
+    modified_dt: new Date(2020, 1, 24 ),
     content: `Helium is a chemical element with status He and atomic number 2. It is a \n colorless, odorless, tasteless, non-toxic, inert, monatomic gas, the first in the noble gas \n group in the periodic table. Its boiling point is the lowest among all the elements.`
   }, {
     report_id: 3,
-    name: 'Lithium',
-    created_dt: new Date(),
+    name: 'Lisithia',
+    created_dt: new Date(2020, 1, 20),
     status: 'approved',
-    modified_dt: new Date(2018, 11, 24, 10),
+    modified_dt: new Date(2020, 1, 24 ),
     content: `Lithium is a chemical element with status Li and atomic number 3. It is a soft, \n silvery-white alkali metal. Under standard conditions, it is the lightest metal and the \n lightest solid element.`
   }, {
     report_id: 4,
-    name: 'Beryllium',
-    created_dt: new Date(),
+    name: 'Baryl',
+    created_dt: new Date(2020, 1, 27 ),
     status: 'rejected',
+    modified_dt: new Date(2020, 1, 29 ),
     content: `Beryllium is a chemical element with status Be and atomic number 4. It is a \n relatively rare element in the universe, usually occurring as a product of the spallation of \n larger atomic nuclei that have collided with cosmic rays.`
   }, {
     report_id: 5,
-    name: 'Boron',
-    created_dt: new Date(),
+    name: 'Baryl',
+    created_dt: new Date(2020, 2, 2 ),
     status: 'rejected',
+    modified_dt: new Date(2020, 2, 2 ),
     content: `Boron is a chemical element with status B and atomic number 5. Produced entirely \n by cosmic ray spallation and supernovae and not by stellar nucleosynthesis, it is a \n low-abundance element in the Solar system and in the Earth's crust.`
   }, {
     report_id: 6,
-    name: 'Carbon',
-    created_dt: new Date(),
+    name: 'Cid',
+    created_dt: new Date(2020, 2, 8 ),
     status: 'pending',
     content: `Carbon is a chemical element with status C and atomic number 6. It is nonmetallic \n and tetravalent—making four electrons available to form covalent chemical bonds. It belongs \n to group 14 of the periodic table.`
   }, {
     report_id: 7,
-    name: 'Nitrogen',
-    created_dt: new Date(),
-    status: 'rejected',
+    name: 'Newton',
+    created_dt: new Date(2020, 3, 10 ),
+    status: 'pending',
     content: `Nitrogen is a chemical element with status N and atomic number 7. It was first \n discovered and isolated by Scottish physician Daniel Rutherford in 1772.`
   }, {
     report_id: 8,
-    name: 'Oxygen',
-    created_dt: new Date(),
-    status: 'rejected',
+    name: 'Olivia',
+    created_dt: new Date(2020, 3, 11 ),
+    status: 'pending',
     content: `Oxygen is a chemical element with status O and atomic number 8. It is a member of \n the chalcogen group on the periodic table, a highly reactive nonmetal, and an oxidizing \n agent that readily forms oxides with most elements as well as with other compounds.`
   }, {
     report_id: 9,
-    name: 'Fluorine',
-    created_dt: new Date(),
+    name: 'Fred',
+    created_dt: new Date(2020, 3, 18 ),
     status: 'rejected',
+    modified_dt: new Date(2020, 3, 20 ),
     content: `Fluorine is a chemical element with status F and atomic number 9. It is the \n lightest halogen and exists as a highly toxic pale yellow diatomic gas at standard \n conditions.`
   }, {
     report_id: 10,
-    name: 'Neon',
-    created_dt: new Date(),
+    name: 'Newton',
+    created_dt: new Date(2020, 4, 30 ),
     status: 'rejected',
+    modified_dt: new Date(2020, 4, 30 ),
     content: `Neon is a chemical element with status Ne and atomic number 10. It is a noble gas. \n Neon is a colorless, odorless, inert monatomic gas under standard conditions, with about \n two-thirds the density of air.`
   },
 ];
